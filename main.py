0ee693cf13180ef8477a59da0ef85d1cbb5c08e5
Smart Xia <835106505@qq.com>
2021-12-20 14:59:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,10 +58,7 @@ if __name__ == '__main__':
     print(res)
     # SERVERCHAN_SECRETKEY = os.environ["SERVERCHAN_SECRETKEY"]
     print('sc_key: ', config.TEST_COOKIE)
-    # if isinstance(config.SERVERCHAN_SECRETKEY,str) and len(config.SERVERCHAN_SECRETKEY)>0:
-
-        print('检测到 SCKEY， 准备推送')
-        push_to_wechat(text = '什么值得买每日签到',
-                        desp = str(res),
-                        secretKey = SERVERCHAN_SECRETKEY)
+    #if isinstance(config.SERVERCHAN_SECRETKEY,str) and len(config.SERVERCHAN_SECRETKEY)>0:
+    print('检测到 SCKEY， 准备推送')
+    push_to_wechat(text = '什么值得买每日签到', desp = str(res),secretKey = config.SERVERCHAN_SECRETKEY)
     print('代码完毕')
